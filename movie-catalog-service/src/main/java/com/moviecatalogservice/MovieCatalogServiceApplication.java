e962ee780b2923e519f021ef50b1f8a8eeb37559
mohamedmoeen <mohamed.moeen166@gmail.com>
2026-04-14 00:43:46 +0200

    implementing grpc

--- movie-catalog-service/src/main/java/com/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+
 public class MovieCatalogServiceApplication {
 
     private final int TIMEOUT = 3000;   // 3 seconds
